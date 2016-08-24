bef3fa19ef9d5cbf3f98e57b72e88f48648a4c60
Christine Abernathy <ca.abernathy@fb.com>
2016-08-24 02:38:01 +0200

    Basic navigation

--- ReactNative/index.android.js
+++ ReactNative/index.android.js
@@ -9,24 +9,43 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator,
 } from 'react-native';
 
 class Quizzer extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        ref='nav'
+        initialRoute={{ name: 'Home' }}
+        renderScene={(route, navigator) => {
+          return (
+            <View style={styles.container}>
+              <Text style={styles.welcome}>
+                Welcome to React Native!
+              </Text>
+              <Text style={styles.instructions}>
+                To get started, edit index.android.js
+              </Text>
+              <Text style={styles.instructions}>
+                Double tap R on your keyboard to reload,{'\n'}
+                Shake or press menu button for dev menu
+              </Text>
+            </View>
+          );
+        }}
+        navigationBar={
+          <Navigator.NavigationBar
+            routeMapper={{
+              LeftButton: () => { },
+              RightButton: () => { },
+              Title: () => { return <Text>Quizzer</Text> },
+            }}
+            style={styles.navBar}
+          />
+        }
+      />
     );
   }
 }
@@ -34,6 +53,7 @@ class Quizzer extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -47,6 +67,9 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  navBar: {
+    backgroundColor: '#eeeeee',
   },
 });
 
